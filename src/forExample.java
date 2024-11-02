726980f2c2db1d8eb51156c369684ba8dc66d274
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2024-11-02 20:03:39 -0300

    gggggggggggggggggggggggggggggggggggg

---
+++ src/forExample.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class forExample {
+    public static void main(String[] args) {
+
+//        sintaxe/regra
+//        for(inicio; condição; incremento){
+//            comando 1
+//            comando 2
+//        }
+
+       Scanner sc = new Scanner(System.in);
+//
+//        int N = sc.nextInt();
+//        int sum = 0;
+//
+//        for (int i = 0; i < N; i++) {
+//            int x = sc.nextInt();
+//            sum += x;
+//        }
+//
+//        System.out.println(sum);
+
+
+        int myNumber = sc.nextInt();
+
+        for(int i = 0; i < myNumber; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+
+    }
+}
