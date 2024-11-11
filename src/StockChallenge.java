630588e26c9d02a475b150748af3af65093965cf
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2024-11-10 21:28:19 -0300

    chore: add constructor Product

--- src/StockChallenge.java
+++ src/StockChallenge.java
@@ -8,15 +8,15 @@ public class StockChallenge {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
-
         System.out.print("Enter product data: ");
         System.out.print("Enter product name: ");
-        product.name = sc.next();
+        String name = sc.next();
         System.out.print("Enter product price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Enter product quantity: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
 
         System.out.printf("Product data: %s, $ %.2f, %s units, Total: $ %.2f%n", product.name, product.price, product.quantity, product.totalValueInStock());
 
