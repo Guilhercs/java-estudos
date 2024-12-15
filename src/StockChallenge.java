6a9599c5e9e95cdc908c709c6efb9af6aa5ef6f3
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2024-12-15 20:33:57 -0300

    chore: vectors

--- src/StockChallenge.java
+++ src/StockChallenge.java
@@ -17,17 +17,20 @@ public class StockChallenge {
 
         //Product product = new Product(name, price, quantity);
 
-        System.out.printf("Product data: %s, $ %.2f, %s units, Total: $ %.2f%n", product.name, product.price, product.quantity, product.totalValueInStock());
+        System.out.printf("Product data: %s, $ %.2f, %s units, Total: $ %.2f%n",
+                product.name, product.price, product.quantity, product.totalValueInStock());
 
         System.out.print("Enter the number of products to be added in stock: ");
         product.addProducts(sc.nextInt());
 
-        System.out.printf("Update data: %s, $ %.2f, %s units, Total: $ %.2f%n", product.name, product.price, product.quantity, product.totalValueInStock());
+        System.out.printf("Update data: %s, $ %.2f, %s units, Total: $ %.2f%n",
+                product.name, product.price, product.quantity, product.totalValueInStock());
 
         System.out.print("Enter the number of products to be removed in stock: ");
         product.removeProducts(sc.nextInt());
 
-        System.out.printf("Update data: %s, $ %.2f, %s units, Total: $ %.2f%n", product.name, product.price, product.quantity, product.totalValueInStock());
+        System.out.printf("Update data: %s, $ %.2f, %s units, Total: $ %.2f%n",
+                product.name, product.price, product.quantity, product.totalValueInStock());
 
         sc.close();
 
